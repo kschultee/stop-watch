9ca03361b69581bac6d2b9d16eaf81ae20109c96
kschultee <kschultee@gmail.com>
2018-02-27 12:45:07 -0800

    Update main.js

--- startTimer/main.js
+++ startTimer/main.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-unused-vars */
-var tenths = 0
-var seconds = 0
-var mins = 0
+var timer {
+  seconds: 0
+  mins: 0
+  tenths: 0
+}
+
 var getTenths = document.getElementById('tenths')
 var getSeconds = document.getElementById('seconds')
 var getMins = document.getElementById('mins')
@@ -13,26 +16,26 @@ start.onclick = function () {
 }
 
 function startTimer() {
-  tenths++
-  if (tenths < 9) {
-    getTenths.textContent = '0' + tenths
+  timer.tenths++
+  if (timer.tenths < 9) {
+    getTenths.textContent = '0' + timer.tenths
   }
-  if (tenths > 9) {
-    getTenths.textContent = tenths
+  if (timer.tenths > 9) {
+    getTenths.textContent = timer.tenths
   }
-  if (tenths > 99) {
-    seconds++
-    getSeconds.textContent = '0' + seconds
-    tenths = 0
+  if (timer.tenths > 99) {
+    timer.seconds++
+    getSeconds.textContent = '0' + timer.seconds
+    timer.tenths = 0
     getTenths.textContent = '0' + 0
   }
-  if (seconds > 9) {
-    getSeconds.textContent = seconds
+  if (timer.seconds > 9) {
+    getSeconds.textContent = timer.seconds
   }
-  if (seconds > 59) {
-    mins++
-    getMins.textContent = '0' + mins
-    seconds = 0
+  if (timer.seconds > 59) {
+    timer.mins++
+    getMins.textContent = '0' + timer.mins
+    timer.seconds = 0
     getSeconds.textContent = '0' + 0
   }
 }
