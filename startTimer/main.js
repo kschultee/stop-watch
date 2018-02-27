e83ec3d385db6604fed04982c9309b8be3124a3d
Kevin Schulte <kschultee@gmail.com>
2018-02-27 14:39:43 -0800

    fixed i think

--- startTimer/main.js
+++ startTimer/main.js
@@ -1,36 +1,42 @@
 /* eslint-disable no-unused-vars */
-var tenths = 0
-var seconds = 0
-var mins = 0
+var timer {
+  seconds: 0
+  mins: 0
+  tenths: 0
+}
+
 var getTenths = document.getElementById('tenths')
 var getSeconds = document.getElementById('seconds')
 var getMins = document.getElementById('mins')
 var start = document.getElementById('start')
+
 var interval
+
 start.onclick = function () {
   interval = setInterval(startTimer, 10)
 }
+
 function startTimer() {
-  tenths++
-  if (tenths < 9) {
-    getTenths.textContent = '0' + tenths
+  timer.tenths++
+  if (timer.tenths < 9) {
+    getTenths.textContent = '0' + timer.tenths
   }
-  if (tenths > 9) {
-    getTenths.textContent = tenths
+  if (timer.tenths > 9) {
+    getTenths.textContent = timer.tenths
   }
-  if (tenths > 99) {
-    seconds++
-    getSeconds.textContent = '0' + seconds
-    tenths = 0
+  if (timer.tenths > 99) {
+    timer.seconds++
+    getSeconds.textContent = '0' + timer.seconds
+    timer.tenths = 0
     getTenths.textContent = '0' + 0
   }
-  if (seconds > 9) {
-    getSeconds.textContent = seconds
+  if (timer.seconds > 9) {
+    getSeconds.textContent = timer.seconds
   }
-  if (seconds > 59) {
-    mins++
-    getMins.textContent = '0' + mins
-    seconds = 0
+  if (timer.seconds > 59) {
+    timer.mins++
+    getMins.textContent = '0' + timer.mins
+    timer.seconds = 0
     getSeconds.textContent = '0' + 0
   }
 }
